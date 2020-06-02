bd25f8abbb46e346ded438e9b33eeccdb8f0ca6f
Peter Hawthorne <peterhawthorne@IONE201554832.local>
2020-06-02 17:11:26 -0500

    add some (meaningless) color

--- static/js/main_map.js
+++ static/js/main_map.js
@@ -10,7 +10,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: mapboxAccessToken
 }).addTo(mspmap);
 
+
+const chlorocolors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a'];
+
+const getColor = (d) => {
+  const idstr = d.toString();
+  const idChar = idstr.charAt(idstr.length-1);
+  return chlorocolors[parseInt(idChar)];
+}
+
+const style = (feature) => {
+  return {
+    fillColor: getColor(feature.properties.OBJECTID),
+    weight: 1,
+    opacity: 0.5,
+    color: 'white',
+    dashArray: '3',
+    fillOpacity: 0.7
+  };
+}
+
 shapeFile = "static/data/census_bg.geojson";
 fetch(shapeFile)
   .then(response => response.json())
-  .then(data => L.geoJson(data).addTo(mspmap))
+  .then(data => L.geoJson(data, {style: style}).addTo(mspmap))
