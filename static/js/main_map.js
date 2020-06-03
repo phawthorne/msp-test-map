1b2d5796504b6798d5fe0ddd4fd364adee5ae65b
Peter Hawthorne <peterhawthorne@IONE201554832.local>
2020-06-03 08:49:36 -0500

    update with layer control and github-only api key

--- static/js/main_map.js
+++ static/js/main_map.js
@@ -1,15 +1,29 @@
 const mapboxAccessToken = 'sk.eyJ1IjoicGhhd3Rob3JuZSIsImEiOiJja2F5aGxxZm8wamVhMnhzOGFiazJ3ajVkIn0.SCkw2StsCcK38QmFmaFJ6w';
-let mspmap = L.map('main-map').setView([44.9778, -93.2650], 11);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+
+const mbAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
+const mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'
+
+const lightMap = L.tileLayer(mbUrl, {
+  attribution: mbAttribution,
+  maxZoom: 18,
+  id: 'mapbox/light-v10',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: mapboxAccessToken
+});
+const streetMap = L.tileLayer(mbUrl, {
+  attribution: mbAttribution,
   maxZoom: 18,
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
   accessToken: mapboxAccessToken
-}).addTo(mspmap);
-
+});
+const baseLayers = {
+  "Grayscale": lightMap,
+  "Streets": streetMap
+};
 
 const chlorocolors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a'];
 
@@ -30,7 +44,21 @@ const style = (feature) => {
   };
 }
 
+let cbOverlay = L.layerGroup();
+let overlays = {"Census blocks": cbOverlay};
+
+
+let mspmap = L.map('main-map', {
+  center: [44.9778, -93.2650],
+  zoom: 11,
+  layers: [lightMap, cbOverlay]
+});
+
 shapeFile = "static/data/census_bg.geojson";
 fetch(shapeFile)
   .then(response => response.json())
-  .then(data => L.geoJson(data, {style: style}).addTo(mspmap))
+  .then(data => {
+    L.geoJson(data, {style: style}).addTo(cbOverlay);
+    L.control.layers(baseLayers, overlays).addTo(mspmap);
+  });
+
